13fd805b5ada2177636decf9ee5d1fd7f70fa100
Alex L. Demidov <alexeydemidov@gmail.com>
2010-12-20 22:28:25 +0300

    add URL Resource object

---
+++ lib/bigdoor/url.rb
@@ -0,0 +1,19 @@
+module BigDoor
+    #
+    # This module provides URL Resource object 
+    # corresponding to /url BigDoor API end point
+    #
+    class URL < Resource
+        def initialize( hash = {} ) 
+            $log.debug( "URL init with hash = #{hash.inspect}")
+            default_values = {
+                'read_only'   => 1,
+                'friendly_id' => nil,
+                'attributes'  => [],
+            }
+            default_values.merge!( hash )
+            $log.debug( "URL default_values = #{default_values.inspect}")
+            super( default_values )
+        end
+    end
+end
